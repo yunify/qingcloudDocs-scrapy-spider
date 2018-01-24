94573c8e0a144cd22ae2fe67bdaead849971ec23
樊尚享 <martinfan@yunify.com>
2018-01-24 14:08:35 +0800

    use sitemap to parse urls

--- qingclouddocs/spiders/docschecker.py
+++ qingclouddocs/spiders/docschecker.py
@@ -1,32 +1,35 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.spiders import Spider
+from scrapy.spiders import SitemapSpider
 from urllib.parse import urlparse
 import validators
+import requests
 
 
-class DocscheckerSpider(Spider):
+class DocscheckerSpider(SitemapSpider):
     name = 'docschecker'
-    start_urls = ['https://docs.qingcloud.com/appcenter1/api/index.html']#'http://localhost:4000']
+    sitemap_urls = ['http://localhost:4000/sitemap.xml']#'http://localhost:4000']
     handle_httpstatus_list = [404]
 
     def parse(self, response):
         if response.status == 404:
             self.logger.error("Found dead link %s" % response.url)
         elif response.headers['Content-Type'] == b'text/html' or response.headers['Content-Type'] == b'text/html; charset=utf-8':
-            self.logger.info("Visiting %s" % response.url)
             items = response.css('a::attr(href)').extract()
             for item in items:
                 if item[0] == '/':
                     baseurl = urlparse(response.url)
                     url = "{}://{}{}".format(baseurl.scheme, baseurl.netloc, item)
-                    yield scrapy.Request(url)
+                    self.validateUrl(response.url, url)
                 elif validators.url(item):
                     if urlparse(item).hostname in ['docs.qingcloud.com', 'localhost']:
-                        yield scrapy.Request(item)
+                        self.validateUrl(response.url,item)
                 elif item == "javascript:;" or item.startswith("mailto:"):
                     pass
                 else:
-                    yield scrapy.Request(response.urljoin(item))
+                    self.validateUrl(response.url,response.urljoin(item))
 
-
+    def validateUrl(self,base,url):
+        r = requests.get(url)
+        if r.status_code == 404:
+            self.logger.error("{} found deadlink {}".format(base, url))
